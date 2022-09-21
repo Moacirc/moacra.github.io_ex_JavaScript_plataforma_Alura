ebcacc8cd6e842631c833c76d7925e71b1808dd0
Moacir Plakitqen <62575674+Moacirc@users.noreply.github.com>
2022-09-20 22:16:33 -0300

    Update calcula-imc.js

--- js/calcula-imc.js
+++ js/calcula-imc.js
@@ -1,5 +1,5 @@
 var titulo = document.querySelector(".titulo");
-titulo.textContent = "Aparecida Nutricionista";
+titulo.textContent = "Moacir Nutricionista";
 
 var pacientes = document.querySelectorAll(".paciente");
 
